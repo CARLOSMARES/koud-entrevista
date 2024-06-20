1e9c5fe701ccd8d54d5218aa594838889d874b8f
CARLOSMARES <46010587+CARLOSMARES@users.noreply.github.com>
2024-06-20 09:23:37 +0000

    Se emṕieza a programar el servicio

--- tvshow/ClientApp/src/app/services/tv-show-api.service.ts
+++ tvshow/ClientApp/src/app/services/tv-show-api.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+
 @Injectable({
   providedIn: 'root'
 })
 export class TvShowApiService {
 
-  constructor() { }
+  constructor(public http: HttpClient) {
+    //this.http = new HttpClient();
+   }
+
+   GetAll(){
+    this.http.get("http://localhost:44498/");
+   }
+
 }
