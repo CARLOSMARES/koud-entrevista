05ba61f4e57768a5dc85498a634059eaff7c2018
CARLOSMARES <46010587+CARLOSMARES@users.noreply.github.com>
2024-06-21 07:18:34 +0000

    Se crean os proyectos desde cero

---
+++ TvShowApi/TvShow.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+public class TvShow{
+
+    [Key]
+    public int Id {get; set;}
+
+    [Required]
+    [MaxLength(100)]
+    public string Name {get; set;}
+
+    public bool Favorite {get; set;}
+    
+}
