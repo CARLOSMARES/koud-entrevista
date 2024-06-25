c6961c44ac5c307e4b39274dab94a8139dc07602
CARLOSMARES <microsystemags@hotmail.com>
2024-06-24 21:46:52 -0600

    se compila la primera build

--- TvShowApi/Program.cs
+++ TvShowApi/Program.cs
@@ -22,6 +22,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.MapGet("/", ()=>"Hola Mundo");
+
 app.MapGet("/api/v1/All", async (TvShowContext context) =>
 {
     return await context.TvShows.ToListAsync();
@@ -29,6 +31,27 @@ app.MapGet("/api/v1/All", async (TvShowContext context) =>
 
 app.MapGet("/api/v1/{id}", async (int id, TvShowContext context) =>{
     return await context.TvShows.FirstOrDefaultAsync(x => x.Id == id);
+});
+
+app.MapGet("/api/v1/name/", async (string name, TvShowContext context) =>{
+    return await context.TvShows.FirstOrDefaultAsync(x => x.Name == name);
+});
+
+app.MapPost("/api/v1/New", async (TvShow tvShow, TvShowContext context) =>{
+    return await context.TvShows.AddAsync(tvShow);
+    context.SaveChanges();
+});
+
+app.MapDelete("/api/v1/{id}", async (int id, TvShowContext context) =>{
+    TvShow entidad = context.TvShows.First(x => x.Id == id);
+    return context.Remove(entidad);
+    context.SaveChanges();
+});
+
+app.MapPut("/api/v1/", async (TvShow tvShow, TvShowContext context) =>{
+    context.Update(tvShow);
+    context.SaveChanges();
+    return tvShow;
 });
 
 app.Run();
